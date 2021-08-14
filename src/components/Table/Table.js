492cc718bf2adcc73edcca0b1c8badcab8fc7444
sfistaq <sfistaq@me.com>
2021-08-14 20:15:58 +0200

    test

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -17,7 +17,7 @@ import Chip from "@material-ui/core/Chip";
 
 import sortArrayObj from "../../utils/sort";
 
-import Head from "../TableHead/TableHead"; // TABLE HEAD
+import Head from "../TableHead/TableHead"; //  HEAD
 
 const useStyles = makeStyles((theme) => ({
   table: {
